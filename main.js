15e760a70ca5fa7613cd57a2d9619dd0227c2bbb
christy cooper betty <ccooper.prsvr@gmail.com>
2023-11-21 21:05:46 -0600

    working on

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 /************************************************** */
 /****************YOUR CODE HERE************* */
+function add(num1, num2){
+console.log(num1 + num2)
+}
+
+function subtract(num1, num2){
+console.log(num1 - num2)    
+}
+
+function multiply(num1, num2){
+console.log(num1 * num2)    
+}
+
+function divide(num1, num2){
+console.log(num1 / num2)
+}
+
+function square(){
+    
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
